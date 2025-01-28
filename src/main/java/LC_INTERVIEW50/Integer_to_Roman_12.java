300e11730406001232c3f48f43c0b8dc0851a6f1
MayurSarkar <76404762+MayurSarkar@users.noreply.github.com>
2025-01-28 14:39:06 +0530

    java files updated

---
+++ src/main/java/LC_INTERVIEW50/Integer_to_Roman_12.java
@@ -0,0 +1,30 @@
+package LC_INTERVIEW50;
+import java.util.*;
+
+public class Integer_to_Roman_12 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int num = scanner.nextInt();
+
+        String roman = intToRoman(num);
+
+        System.out.println("Roman numeral: " + roman);
+    }
+
+    public static String intToRoman(int num) {
+        StringBuilder result = new StringBuilder();
+
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                result.append(symbols[i]);
+                num -= values[i];
+            }
+        }
+        return result.toString();
+    }
+}
